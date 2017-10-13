f56db9542ccb61007f3337183729e57999b0d3bb
Jari Saarela <jari.saarela@arcusys.fi>
2017-10-13 09:32:26 +0300

    Poistetaan tietokantadata reposta

--- src/main/resources/db/migration/v4__paatoskierros2017_insert.sql
+++
@@ -1,6 +0,0 @@
-
--- lisätään uusi esitysmalli
-INSERT INTO oiva.esitysmalli(templatepath,luoja,luontipvm) VALUES ('paatoskierros2017','oiva','2017-10-13 09:05:28.53536');
-
--- päivitetään uusien lupien päätöskierroksen esitysmalli
-UPDATE oiva.paatoskierros SET esitysmalli_id = 9 WHERE id = 19;
