a2f6618fc38ea7983fad897eea5ff6137469c781
Aki Heikkinen <aki.heikkinen@arcusys.fi>
2017-12-03 12:20:23 +0200

    development version is not updated on release

--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ if [[ $1 == "publish" ]]; then
 		    mvn release:clean
 		fi
 		git merge --no-ff -m "Merge latest development version" develop
-		mvn release:prepare -Dusername=git -P dev
+		mvn release:prepare -DupdateWorkingCopyVersions=false -Dusername=git -P dev
 	else
 		echo "Release preparation cancelled"
 	fi
@@ -26,5 +26,5 @@ else
 	if [ -f ./pom.xml.tag ]; then
 	    mvn release:clean
 	fi
-	mvn release:prepare -DdryRun=true -Dusername=git -P dev
+	mvn release:prepare -DupdateWorkingCopyVersions=false -DdryRun=true -Dusername=git -P dev
 fi
