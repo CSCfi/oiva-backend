9ebc863da0ff987341cd9df4670e30d66b3a4805
Jari Saarela <jari.saarela@arcusys.fi>
2017-12-08 08:24:43 +0200

    Lisää koodistoversio maarays tietomalliin

--- src/main/generated/jooq/fi/minedu/oiva/backend/jooq/tables/Maarays.java
+++ src/main/generated/jooq/fi/minedu/oiva/backend/jooq/tables/Maarays.java
@@ -40,7 +40,7 @@ import org.jooq.impl.TableImpl;
 @SuppressWarnings({ "all", "unchecked", "rawtypes" })
 public class Maarays extends TableImpl<MaaraysRecord> {
 
-    private static final long serialVersionUID = -1092980392;
+    private static final long serialVersionUID = -1064705948;
 
     /**
      * The reference instance of <code>oiva.maarays</code>
@@ -119,6 +119,11 @@ public class Maarays extends TableImpl<MaaraysRecord> {
      * The column <code>oiva.maarays.paivityspvm</code>.
      */
     public final TableField<MaaraysRecord, Timestamp> PAIVITYSPVM = createField("paivityspvm", org.jooq.impl.SQLDataType.TIMESTAMP, this, "");
+
+    /**
+     * The column <code>oiva.maarays.koodistoversio</code>.
+     */
+    public final TableField<MaaraysRecord, Integer> KOODISTOVERSIO = createField("koodistoversio", org.jooq.impl.SQLDataType.INTEGER.nullable(false).defaultValue(org.jooq.impl.DSL.field("7", org.jooq.impl.SQLDataType.INTEGER)), this, "");
 
     /**
      * Create a <code>oiva.maarays</code> table reference
---
+++ src/main/resources/db/migration/V4__add_koodistoversio_to_maarays.sql
@@ -0,0 +1,1 @@
+ALTER TABLE oiva.maarays ADD koodistoversio int NOT NULL default 7;
