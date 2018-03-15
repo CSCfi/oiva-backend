1d784500b182ee1f1f3129605e536ef1409ffa4f
Jari Saarela <jari.saarela@arcusys.fi>
2018-03-15 14:43:25 +0200

    Rajapinta avoimille päätöskierroksille.

--- src/main/java/fi/minedu/oiva/backend/service/PaatoskierrosService.java
+++ src/main/java/fi/minedu/oiva/backend/service/PaatoskierrosService.java
@@ -2,10 +2,14 @@ package fi.minedu.oiva.backend.service;
 
 import fi.minedu.oiva.backend.entity.Paatoskierros;
 import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.Collection;
+import java.util.Date;
 
 import static fi.minedu.oiva.backend.jooq.Tables.PAATOSKIERROS;
 
@@ -18,4 +22,11 @@ public class PaatoskierrosService {
     public Collection<Paatoskierros> getAll() { // TODO: IS THIS NEEDED?
         return dsl.select(PAATOSKIERROS.fields()).from(PAATOSKIERROS).fetchInto(Paatoskierros.class);
     }
+
+    public Collection<Paatoskierros> getOpen() { // TODO: IS THIS NEEDED?
+        return dsl.select(PAATOSKIERROS.fields()).from(PAATOSKIERROS)
+                .where( (PAATOSKIERROS.ALKUPVM.le(DSL.currentDate()).and(PAATOSKIERROS.LOPPUPVM.greaterOrEqual(DSL.currentDate()))))
+                .fetchInto(Paatoskierros.class);
+    }
+
 }
