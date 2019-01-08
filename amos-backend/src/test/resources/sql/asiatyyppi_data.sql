d9e0d6bd61a9f709d88643b7057e87d058213a74
Valtteri Rouvinen <valtteri.rouvinen@valamis.com>
2019-01-08 09:14:41 +0200

    Added amos backend integration tests.

---
+++ amos-backend/src/test/resources/sql/asiatyyppi_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO oiva.asiatyyppi (id, tunniste, selite, uuid)
+VALUES (1, 'UUSI', '{"fi": "Uusi lupa"}', 'cc32d88a-43b6-11e8-b2ef-005056aa0e66');
+INSERT INTO oiva.asiatyyppi (id, tunniste, selite, uuid)
+VALUES (2, 'MUUTOS', '{"fi": "Luvan muutos"}', 'cc32ddb2-43b6-11e8-b2ef-005056aa0e66');
+INSERT INTO oiva.asiatyyppi (id, tunniste, selite, uuid)
+VALUES (3, 'AK', '{"fi": "Ammattikoulu"}', 'cc32deac-43b6-11e8-b2ef-005056aa0e66');
+INSERT INTO oiva.asiatyyppi (id, tunniste, selite, uuid)
+VALUES (4, 'LUKIO', '{"fi": "Lukio"}', 'cc32df88-43b6-11e8-b2ef-005056aa0e66');
+INSERT INTO oiva.asiatyyppi (id, tunniste, selite, uuid)
+VALUES (5, 'PERUUTUS', '{"fi": "Luvan peruutus"}', '995a6970-c569-11e8-adc0-005056aa0e66');
---
+++ amos-backend/src/test/resources/sql/lupa_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO oiva.lupa (id, edellinen_lupa_id, paatoskierros_id, lupatila_id, asiatyyppi_id, diaarinumero,
+                       jarjestaja_ytunnus, jarjestaja_oid, alkupvm, loppupvm, paatospvm, meta, maksu, luoja, luontipvm,
+                       paivittaja, paivityspvm, uuid)
+VALUES (1, null, 1, 3, 1, '11/111/2019', '1111111-1', '1.2.246.562.10.22002142283', '2019-01-01', null, '2018-12-31',
+        '{"ministeri": "Testi Ministeri", "esittelija": "Testi Esittelijä"}',
+        false, 'oiva', '2017-04-26 13:49:05.834530', null, null, 'cc3c1d00-43b6-11e8-b2ef-005056aa0e66');
+INSERT INTO oiva.lupa (id, edellinen_lupa_id, paatoskierros_id, lupatila_id, asiatyyppi_id, diaarinumero,
+                       jarjestaja_ytunnus, jarjestaja_oid, alkupvm, loppupvm, paatospvm, meta, maksu, luoja, luontipvm,
+                       paivittaja, paivityspvm, uuid)
+VALUES (2, null, 1, 3, 1, '22/222/2019', '2222222-2', '1.2.246.562.10.48291859840', '2019-01-01', null, '2018-12-31',
+        '{"ministeri": "Testi Ministeri", "esittelija": "Testi Esittelijä"}',
+        false, 'oiva', '2017-08-28 12:58:22.066839', null, null, 'cc3c20a2-43b6-11e8-b2ef-005056aa0e66');
+INSERT INTO oiva.lupa (id, edellinen_lupa_id, paatoskierros_id, lupatila_id, asiatyyppi_id, diaarinumero,
+                       jarjestaja_ytunnus, jarjestaja_oid, alkupvm, loppupvm, paatospvm, meta, maksu, luoja, luontipvm,
+                       paivittaja, paivityspvm, uuid)
+VALUES (3, null, 1, 3, 1, '33/333/2019', '3333333-3', '1.2.246.562.10.27577940193', '2019-01-01', null, '2018-12-31',
+        '{"ministeri": "Testi Ministeri", "esittelija": "Testi Esittelijä"}',
+        false, 'oiva', '2017-09-20 09:25:22.066839', null, null, 'cc3c21ba-43b6-11e8-b2ef-005056aa0e66');
+INSERT INTO oiva.lupa (id, edellinen_lupa_id, paatoskierros_id, lupatila_id, asiatyyppi_id, diaarinumero,
+                       jarjestaja_ytunnus, jarjestaja_oid, alkupvm, loppupvm, paatospvm, meta, maksu, luoja, luontipvm,
+                       paivittaja, paivityspvm, uuid)
+VALUES (4, null, 1, 3, 1, '44/444/2019', '4444444-4', '1.2.246.562.10.16141554515', '2019-01-01', null, '2018-12-31',
+        '{"ministeri": "Testi Ministeri", "esittelija": "Testi Esittelijä"}',
+        false, 'oiva', '2017-09-21 09:48:22.066839', null, null, 'cc3c2296-43b6-11e8-b2ef-005056aa0e66');
