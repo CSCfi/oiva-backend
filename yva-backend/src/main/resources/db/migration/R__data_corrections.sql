cf91fc0b691102532f1bfe10b6df8569a92748a4
Valtteri Rouvinen <valtteri.rouvinen@valamis.com>
2019-10-14 12:53:53 +0300

    CSCKUJA-93 Added oppilaitos oid for lukioerityinenkoulutustehtava.

--- yva-backend/src/main/resources/db/migration/R__data_corrections.sql
+++ yva-backend/src/main/resources/db/migration/R__data_corrections.sql
@@ -226,3 +226,112 @@ WHERE l.id = m.lupa_id
     SELECT DISTINCT l.id FROM lupa l LEFT JOIN maarays m2 on l.id = m2.lupa_id
     WHERE m2.koodisto = 'koulutusmuoto' AND m2.koodiarvo = '2'
     );
+
+-- Add oppilaitos oid to lukioerityinenkoulutustehtava maarays
+CREATE TEMP TABLE temp_oid
+(
+    diaari VARCHAR,
+    kj     VARCHAR,
+    nimi   VARCHAR,
+    oid    VARCHAR
+);
+
+INSERT INTO temp_oid (diaari, kj, nimi, oid)
+VALUES ('55/530/2017', 'Englantilaisen koulun säätiö sr', 'Englantilainen koulu', '1.2.246.562.10.29138879688'),
+       ('49/530/2017', 'Espoon kaupunki', 'Tapiolan lukio', '1.2.246.562.10.90064668321'),
+       ('49/530/2017', 'Espoon kaupunki', 'Olarin lukio', '1.2.246.562.10.52129505609'),
+       ('49/530/2017', 'Espoon kaupunki', 'Etelä-Tapiolan lukio', '1.2.246.562.10.640696416910'),
+       ('49/530/2017', 'Espoon kaupunki', 'Mattlidens gymnasium', '1.2.246.562.10.72753205474'),
+       ('21/530/2018', 'Helsingin Kansainvälisen Koulun Vanhempain yhdistys ry',
+        'Helsingin kansainvälinen koulu (International School of Helsinki)', '1.2.246.562.10.67636414343'),
+       ('43/530/2017', 'Helsingin kaupunki', 'Sibelius-lukio', '1.2.246.562.10.16790925842'),
+       ('43/530/2017', 'Helsingin kaupunki', 'Helsingin kuvataidelukio', '1.2.246.562.10.88337368331'),
+       ('43/530/2017', 'Helsingin kaupunki', 'Mäkelänrinteen lukio', '1.2.246.562.10.32812335316'),
+       ('43/530/2017', 'Helsingin kaupunki', 'Kallion lukio', '1.2.246.562.10.69408568777'),
+       ('43/530/2017', 'Helsingin kaupunki', 'Helsingin luonnontiedelukio', '1.2.246.562.10.54336251563'),
+       ('43/530/2017', 'Helsingin kaupunki', 'Ressun lukio', '1.2.246.562.10.62858797335'),
+       ('43/530/2017', 'Helsingin kaupunki', 'Brändö gymnasium', '1.2.246.562.10.18031194469'),
+       ('43/530/2017', 'Helsingin kaupunki', 'Tölö gymnasium', '1.2.246.562.10.17856506963'),
+       ('31/530/2017', 'Helsingin Ranskalais-Suomalainen koulu', 'Helsingin ranskalais-suomalainen koulu',
+        '1.2.246.562.10.87294891194'),
+       ('46/530/2017',
+        'Helsingin Rudolf Steiner-Koulun kannatusyhdistys - Understödsföreningen för Rudolf Steiner Skolan i Helsingfors r.y.',
+        'Helsingin, Espoon ja Vantaan Rudolf Steiner -koulut', '1.2.246.562.10.82407007176'),
+       ('26/530/2017', 'Hyvinkään kaupunki', 'Hyvinkään Sveitsin lukio', '1.2.246.562.10.50435734616'),
+       ('85/530/2017', 'Imatran kaupunki', 'Imatran yhteislukio', '1.2.246.562.10.32863187889'),
+       ('8/530/2016', 'Imatran kaupunki', 'Imatran yhteislukio', '1.2.246.562.10.32863187889'),
+       ('85/530/2017', 'Imatran kaupunki', 'Lappeenrannan Lyseon lukio', '1.2.246.562.10.62698051406'),
+       ('8/530/2016', 'Imatran kaupunki', 'Lappeenrannan Lyseon lukio', '1.2.246.562.10.62698051406'),
+       ('78/530/2017', 'Itä-Suomen suomalais-venäläisen koulun säätiö sr',
+        'Itä-Suomen suomalais-venäläisen koulun lukiot', '1.2.246.562.10.87186754146'),
+       ('21/530/2017', 'Joensuun kaupunki', 'Joensuun lyseon lukio', '1.2.246.562.10.50785625307'),
+       ('21/530/2017', 'Joensuun kaupunki', 'Joensuun yhteiskoulun lukio', '1.2.246.562.10.660024022710'),
+       ('42/530/2017', 'Jyväskylän Koulutuskuntayhtymä', 'Schildtin lukio', '1.2.246.562.10.26026360659'),
+       ('42/530/2017', 'Jyväskylän Koulutuskuntayhtymä', 'Jyväskylän Lyseon lukio', '1.2.246.562.10.73954234818'),
+       ('25/530/2017', 'Järvenpään kaupunki', 'Järvenpään lukio', '1.2.246.562.10.44551697236'),
+       ('28/530/2017', 'Kajaanin kaupunki', 'Kajaanin lukio', '1.2.246.562.10.99682778883'),
+       ('58/530/2017', 'Kaustisen kunta', 'Kaustisen musiikkilukio', '1.2.246.562.10.19424913812'),
+       ('65/530/2017', 'Kokkolan kaupunki', 'Kokkolan suomalainen lukio', '1.2.246.562.10.74849589636'),
+       ('7/530/2018', 'Koulutuskuntayhtymä Tavastia', 'Kaurialan lukio', '1.2.246.562.10.94085708306'),
+       ('78/530/2015', 'Kouluyhdistys Pestalozzi Schulverein Skolföreningen r.y.',
+        'Helsingin saksalainen koulu (Deutsche Schule Helsinki)', '1.2.246.562.10.45093614456'),
+       ('81/530/2017', 'Kuopion kaupunki', 'Kuopion taidelukio Lumit', '1.2.246.562.10.35726824598'),
+       ('81/530/2017', 'Kuopion kaupunki', 'Kuopion klassillinen lukio', '1.2.246.562.10.54527283037'),
+       ('81/530/2017', 'Kuopion kaupunki', 'Kuopion Lyseon lukio', '1.2.246.562.10.64812964803'),
+       ('66/530/2017', 'Kuortaneen kunta', 'Kuortaneen lukio', '1.2.246.562.10.53856958656'),
+       ('82/530/2017', 'Lahden kaupunki', 'Tiirismaan lukio', '1.2.246.562.10.80202766442'),
+       ('82/530/2017', 'Lahden kaupunki', 'Kannaksen lukio', '1.2.246.562.10.22131645974'),
+       ('82/530/2017', 'Lahden kaupunki', 'Lahden lyseo', '1.2.246.562.10.54832487356'),
+       ('82/530/2017', 'Lahden kaupunki', 'Kannaksen lukio', '1.2.246.562.10.22131645974'),
+       ('105/530/2017', 'Lahden Rudolf Steiner-Koulun kannatusyhdistys ry', 'Lahden Rudolf Steiner-koulu',
+        '1.2.246.562.10.62505298505'),
+       ('71/530/2017', 'Lapinlahden kunta', 'Lapinlahden Lukio ja Kuvataidekoulu', '1.2.246.562.10.37255973804'),
+       ('45/530/2017', 'Lauttasaaren yhteiskoulun kannatusyhdistys ry', 'Kansainvälisen liiketoiminnan lukio',
+        '1.2.246.562.10.27885810138'),
+       ('32/530/2017', 'Oulun kaupunki', 'Madetojan musiikkilukio', '1.2.246.562.10.87924925768'),
+       ('79/530/2016', 'Oulun kaupunki', 'Madetojan musiikkilukio', '1.2.246.562.10.87924925768'),
+       ('32/530/2017', 'Oulun kaupunki', 'Kastellin lukio', '1.2.246.562.10.62903818292'),
+       ('79/530/2016', 'Oulun kaupunki', 'Kastellin lukio', '1.2.246.562.10.62903818292'),
+       ('32/530/2017', 'Oulun kaupunki', 'Oulun Lyseon Lukio', '1.2.246.562.10.60110180753'),
+       ('79/530/2016', 'Oulun kaupunki', 'Oulun Lyseon Lukio', '1.2.246.562.10.60110180753'),
+       ('80/530/2017', 'Pohjois-Haagan yhteiskoulu oy', 'Pohjois-Haagan yhteiskoulu', '1.2.246.562.10.62657446472'),
+       ('48/530/2017', 'Porin kaupunki', 'Porin suomalaisen yhteislyseon lukio', '1.2.246.562.10.52943623646'),
+       ('37/530/2017', 'Rovaniemen kaupunki', 'Ounasvaaran lukio', '1.2.246.562.10.98026827554'),
+       ('37/530/2017', 'Rovaniemen kaupunki', 'Lyseonpuiston lukio', '1.2.246.562.10.67444735269'),
+       ('54/530/2017', 'Savonlinnan kaupunki', 'Savonlinnan Taidelukio', '1.2.246.562.10.23185052984'),
+       ('61/530/2017', 'Seinäjoen kaupunki', 'Nurmon lukio', '1.2.246.562.10.98901202204'),
+       ('16/530/2017', 'Sotkamon kunta', 'Sotkamon lukio', '1.2.246.562.10.40463601514'),
+       ('40/530/2017', 'Sotkamon kunta', 'Helsingin Suomalainen Yhteiskoulu', '1.2.246.562.10.419440347710'),
+       ('53/530/2017', 'Suomalais-Venäläinen koulu', 'Suomalais-venäläinen koulu', '1.2.246.562.10.37294025582'),
+       ('69/530/2017', 'Tampereen kaupunki', 'Hatanpään lukio', '1.2.246.562.10.310308081210'),
+       ('69/530/2017', 'Tampereen kaupunki', 'Tammerkosken lukio', '1.2.246.562.10.27933132972'),
+       ('69/530/2017', 'Tampereen kaupunki', 'Sammon keskuslukio', '1.2.246.562.10.55003609967'),
+       ('69/530/2017', 'Tampereen kaupunki', 'Tampereen klassillinen lukio', '1.2.246.562.10.48400886551'),
+       ('69/530/2017', 'Tampereen kaupunki', 'Tampereen lyseon lukio', '1.2.246.562.10.97022525972'),
+       ('75/530/2017', 'Tampereen Steiner-Kouluyhdistys r.y.', 'Tampereen ja Oulun Rudolf Steiner -koulut',
+        '1.2.246.562.10.38718435183'),
+       ('20/530/2017', 'Tampereen Yhteiskoulun Säätiö sr', 'Tampereen yhteiskoulun lukio',
+        '1.2.246.562.10.90831505285'),
+       ('39/530/2017', 'Turun kaupunki', 'Puolalanmäen lukio', '1.2.246.562.10.56492601382'),
+       ('39/530/2017', 'Turun kaupunki', 'Kerttulin lukio', '1.2.246.562.10.93417690927'),
+       ('39/530/2017', 'Turun kaupunki', 'Turun klassillinen lukio', '1.2.246.562.10.72046111935'),
+       ('39/530/2017', 'Turun kaupunki', 'Turun Suomalaisen Yhteiskoulun lukio', '1.2.246.562.10.71782870324'),
+       ('38/530/2017', 'Turun yliopisto', 'Turun normaalikoulun lukio', '1.2.246.562.10.15598319315'),
+       ('76/530/2017', 'Vantaan kaupunki', 'Vaskivuoren lukio', '1.2.246.562.10.88883324092'),
+       ('76/530/2017', 'Vantaan kaupunki', 'Martinlaakson lukio', '1.2.246.562.10.37651195473'),
+       ('76/530/2017', 'Vantaan kaupunki', 'Tikkurilan lukio', '1.2.246.562.10.23097497426'),
+       ('29/530/2017', 'Viipurin Reaalikoulu Oy', 'Maunulan yhteiskoulu ja Helsingin matematiikkalukio',
+        '1.2.246.562.10.54654459491'),
+       ('67/530/2017', 'Vöyrin kunta', 'Vörå samgymnasium', '1.2.246.562.10.57852647106'),
+       ('70/530/2017', 'Ylöjärven kaupunki', 'Ylöjärven lukio', '1.2.246.562.10.81922611934'),
+       ('52/530/2017', 'Åbo Akademi', 'Vasa övningsskolas gymnasium', '1.2.246.562.10.68073671569');
+
+UPDATE maarays m
+SET org_oid = o.oid
+FROM temp_oid o,
+     lupa l
+WHERE l.diaarinumero = o.diaari
+  AND l.id = m.lupa_id
+  AND TEXT(m.meta) LIKE '%' || o.nimi || '%';
+
+DROP TABLE temp_oid;
