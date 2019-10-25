19d2993c6ed37d0d8e9a3966dad621b1b8c67fd3
Valtteri Rouvinen <valtteri.rouvinen@valamis.com>
2019-10-25 10:26:12 +0300

    CSCKUJA-166 Multiple users with esittelija role is now allowed to login.

--- oiva-core/src/test/scala/fi/minedu/oiva/backend/core/security/OivaUserDetailsServiceTest.scala
+++ oiva-core/src/test/scala/fi/minedu/oiva/backend/core/security/OivaUserDetailsServiceTest.scala
@@ -3,9 +3,9 @@ package fi.minedu.oiva.backend.core.security
 import java.util.{Collections, Optional}
 
 import fi.minedu.oiva.backend.BaseSuite
+import fi.minedu.oiva.backend.core.service.OpintopolkuService
 import fi.minedu.oiva.backend.model.entity.opintopolku.{KayttajaKayttooikeus, Kayttooikeus, OrganisaatioKayttooikeus}
 import fi.minedu.oiva.backend.model.security.annotations.OivaAccess
-import fi.minedu.oiva.backend.core.service.OpintopolkuService
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -19,7 +19,7 @@ class OivaUserDetailsServiceTest extends BaseSuite {
   test("Load user details by username") {
     val username = "test"
     val orgOid = "1.1.1.1111"
-    val mockOpintopolku = mockOpintopolkuService(username, orgOid)
+    val mockOpintopolku = mockOpintopolkuService(username, orgOid, OivaAccess.Context_Esittelija)
     val mockRegistry = mock[SessionRegistry]
 
     val service = new OivaUserDetailsService(mockOpintopolku, mockRegistry)
@@ -39,16 +39,16 @@ class OivaUserDetailsServiceTest extends BaseSuite {
   }
 
 
-  test("Load user details by username when organisation has already another editor logged in.") {
+  test("Decrease role when same organization has already logged in user with role 'Kayttaja' or 'Nimenkirjoittaja'") {
     val username = "test"
     val orgOid = "1.1.1.1111"
-    val mockOpintopolku = mockOpintopolkuService(username, orgOid)
+    val mockOpintopolku = mockOpintopolkuService(username, orgOid, OivaAccess.Context_Nimenkirjoittaja)
     val mockRegistry = mock[SessionRegistry]
 
     val list = Collections.singletonList(new OivaUserDetails("another_test", "",
-      Collections.singleton(new SimpleGrantedAuthority(OivaAccess.Role_Esittelija)), orgOid, false).asInstanceOf[Object])
-    when(mockRegistry.getAllPrincipals).thenReturn(
-      list)
+      Collections.singleton(new SimpleGrantedAuthority(OivaAccess.Role_Kayttaja)), orgOid, false)
+      .asInstanceOf[Object])
+    when(mockRegistry.getAllPrincipals).thenReturn(list)
 
     val service = new OivaUserDetailsService(mockOpintopolku, mockRegistry)
     val user = service.loadUserByUsername(username)
@@ -68,16 +68,48 @@ class OivaUserDetailsServiceTest extends BaseSuite {
     assert(oivaUser.isPermissionsDecreased, "Permission decreased flag should be true")
   }
 
+  test("Allow multiple users with 'Esittelija'-role logged in without decreasing permission.") {
+    val username = "test"
+    val orgOid = "1.1.1.1111"
+    val mockOpintopolku = mockOpintopolkuService(username, orgOid, OivaAccess.Context_Esittelija)
+    val mockRegistry = mock[SessionRegistry]
+
+    val list = Collections.singletonList(new OivaUserDetails("another_test", "",
+      Collections.singleton(new SimpleGrantedAuthority(OivaAccess.Role_Esittelija)), orgOid, false)
+      .asInstanceOf[Object])
+    when(mockRegistry.getAllPrincipals).thenReturn(
+      list)
+
+    val service = new OivaUserDetailsService(mockOpintopolku, mockRegistry)
+    val user = service.loadUserByUsername(username)
+    assert(user != null, "User should not be null")
+    assert(user.isInstanceOf[OivaUserDetails], "User must be instance of OivaUserDetails")
+    val oivaUser = user.asInstanceOf[OivaUserDetails]
+    assertResult(username) {
+      oivaUser.getUsername
+    }
+    assertResult(orgOid) {
+      oivaUser.getOrganisationOid
+    }
+
+    assertResult(Seq(OivaAccess.Role_Application, OivaAccess.Role_Esittelija)) {
+      oivaUser.getAuthorities.asScala.map(a => a.getAuthority)
+    }
+    assert(!oivaUser.isPermissionsDecreased, "Permission decreased flag should be false")
+  }
+
   test("Case when organisation has already _same_ user logged in.") {
     val username = "test"
     val orgOid = "1.1.1.1111"
-    val mockOpintopolku = mockOpintopolkuService(username, orgOid)
+    val mockOpintopolku = mockOpintopolkuService(username, orgOid, OivaAccess.Context_Esittelija)
     val mockRegistry = mock[SessionRegistry]
 
     val list = List(new OivaUserDetails(username, "",
-      Collections.singleton(new SimpleGrantedAuthority(OivaAccess.Role_Esittelija)), orgOid, false).asInstanceOf[AnyRef],
+      Collections.singleton(new SimpleGrantedAuthority(OivaAccess.Role_Esittelija)), orgOid, false)
+      .asInstanceOf[AnyRef],
       new OivaUserDetails(username + "_another", "",
-        Collections.singleton(new SimpleGrantedAuthority(OivaAccess.Role_Katselija)), orgOid, false).asInstanceOf[AnyRef])
+        Collections.singleton(new SimpleGrantedAuthority(OivaAccess.Role_Katselija)), orgOid, false)
+        .asInstanceOf[AnyRef])
 
     when(mockRegistry.getAllPrincipals).thenReturn(list.asJava)
 
@@ -87,14 +119,14 @@ class OivaUserDetailsServiceTest extends BaseSuite {
     assertResult(Seq(OivaAccess.Role_Application, OivaAccess.Role_Esittelija)) {
       oivaUser.getAuthorities.asScala.map(a => a.getAuthority)
     }
-    assert(!oivaUser.isPermissionsDecreased, "Permission decreased flag should be true")
+    assert(!oivaUser.isPermissionsDecreased, "Permission decreased flag should be false")
   }
 
-  private def mockOpintopolkuService(username: String, orgOid: String) = {
+  private def mockOpintopolkuService(username: String, orgOid: String, accessRight: String) = {
     val mockOpintopolku = mock[OpintopolkuService]
     var orgKayttooikeusList: ArrayBuffer[OrganisaatioKayttooikeus] = ArrayBuffer()
     var kayttooikeusList: ArrayBuffer[Kayttooikeus] = ArrayBuffer()
-    kayttooikeusList += Kayttooikeus(OivaAccess.Context_Oiva, OivaAccess.Context_Esittelija)
+    kayttooikeusList += Kayttooikeus(OivaAccess.Context_Oiva, accessRight)
 
     orgKayttooikeusList += OrganisaatioKayttooikeus(orgOid, kayttooikeusList.asJava)
     when(mockOpintopolku.getKayttajaKayttooikeus(matches(username)))
