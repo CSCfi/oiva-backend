00ea8c30ccafad7c93ae095cc6baaa8b034ddde3
Valtteri Rouvinen <valtteri.rouvinen@valamis.com>
2019-11-18 11:39:59 +0200

    CSCOIVA-999 Savon Koulutusyhtymä lupa changes.

---
+++ amos-model/src/main/resources/db/migration/V2.10__Savon_kky_muutos.sql
@@ -0,0 +1,94 @@
+-- End existing lupa for Savon koulutuskuntayhtymä (1852679-9)
+UPDATE lupa
+SET loppupvm = '2019-12-31'
+WHERE jarjestaja_ytunnus = '1852679-9'
+  AND diaarinumero = '98/531/2017';
+
+-- Create lupahistoria row
+INSERT INTO lupahistoria
+(diaarinumero,
+ ytunnus,
+ oid,
+ maakunta,
+ tila,
+ voimassaoloalkupvm,
+ voimassaololoppupvm,
+ paatospvm,
+ filename)
+VALUES ('98/531/2017', '1852679-9', '1.2.246.562.10.99191194051',
+        'Pohjois-Savo',
+        'Järjestämisluvan muutos', '2018-01-01', '2019-12-31', '2017-10-06',
+        '98-531-2017.pdf');
+
+WITH new_lupa AS (
+    -- Create new lupa for Savon koulutuskuntayhtymä (1852679-9)
+    INSERT INTO lupa (paatoskierros_id, lupatila_id, asiatyyppi_id,
+                      diaarinumero, jarjestaja_ytunnus, jarjestaja_oid, alkupvm, loppupvm, paatospvm,
+                      meta, maksu, luoja, paivittaja, paivityspvm)
+        VALUES (19,
+                3,
+                2,
+                '37/531/2019',
+                '1852679-9',
+                '1.2.246.562.10.99191194051',
+                '2020-01-01',
+                null,
+                '2019-12-16',
+                '{
+                  "ministeri": "Li Andersson",
+                  "esittelija": "Anne Mårtensson",
+                  "ministeri_nimike": "Opetusministeri",
+                  "esittelija_nimike": "Opetusneuvos"
+                }',
+                false,
+                'oiva',
+                null,
+                null) RETURNING id
+),
+     l_liite AS (
+         -- Create lupa liite
+         INSERT INTO liite (nimi, polku, luoja, tyyppi, kieli)
+             VALUES ('Savon koulutuskuntayhtymä',
+                     'paatosKirjeet2019/Savon_koulutuskuntayhtymä.pdf',
+                     'oiva',
+                     'paatosKirje',
+                     'fi') RETURNING id
+     ),
+     link AS (
+         -- Create lupa_liite link
+         INSERT INTO lupa_liite (lupa_id, liite_id) SELECT new_lupa.id, l_liite.id
+                                                    FROM new_lupa,
+                                                         l_liite
+     ),
+     new_maarays AS (
+         -- Copy maarays for new lupa
+         INSERT
+             INTO maarays (lupa_id, kohde_id, koodisto, koodiarvo, arvo, maaraystyyppi_id, meta, koodistoversio)
+                 SELECT new_lupa.id,
+                        m.kohde_id,
+                        m.koodisto,
+                        m.koodiarvo,
+                        m.arvo,
+                        m.maaraystyyppi_id,
+                        m.meta,
+                        -- Update koulutus koodisto version number
+                        CASE WHEN m.koodisto = 'koulutus' THEN 12 ELSE m.koodistoversio END
+                 FROM new_lupa,
+                      maarays m
+                          LEFT JOIN lupa l ON m.lupa_id = l.id
+                 WHERE l.jarjestaja_ytunnus = '1852679-9'
+                   AND l.diaarinumero = '98/531/2017'
+     )
+
+SELECT *
+FROM new_lupa;
+
+-- Update opiskelijamäärä
+UPDATE maarays m
+SET arvo = 40
+FROM lupa l
+WHERE l.id = m.lupa_id
+  AND l.diaarinumero = '37/531/2019'
+  AND m.koodisto = 'oivamuutoikeudetvelvollisuudetehdotjatehtavat'
+  AND m.koodiarvo = '2'
+  AND m.arvo = '27';
