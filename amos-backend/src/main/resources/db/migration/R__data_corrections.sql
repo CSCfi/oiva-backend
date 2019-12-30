5f969fe20bf2be9c125c0dc2f42cfdb4d20dccda
Jermu Mäki <jermu.maki@netum.fi>
2019-12-30 13:31:07 +0200

    Added koodistoversio to kuljettajakoulutus migration

--- amos-backend/src/main/resources/db/migration/R__data_corrections.sql
+++ amos-backend/src/main/resources/db/migration/R__data_corrections.sql
@@ -7,5 +7,7 @@ update lupa set loppupvm='2018-12-31' where jarjestaja_ytunnus = '2064886-7' and
 update lupa set loppupvm='2019-05-31' where jarjestaja_ytunnus='0986820-1' and loppupvm is null;
 
 -- update maarays code values altered in 2019/12
-update maarays set koodiarvo = '1' where koodisto = 'kuljettajakoulutus' and koodiarvo in ('3', '5');
-update maarays set koodiarvo = '2' where koodisto = 'kuljettajakoulutus' and koodiarvo in ('4', '6');
+update maarays set koodiarvo = '1', koodistoversio='2' where koodisto = 'kuljettajakoulutus' and koodiarvo='3';
+update maarays set koodiarvo = '1', koodistoversio='3' where koodisto = 'kuljettajakoulutus' and koodiarvo='5';
+update maarays set koodiarvo = '2', koodistoversio='2' where koodisto = 'kuljettajakoulutus' and koodiarvo='4';
+update maarays set koodiarvo = '2', koodistoversio='3' where koodisto = 'kuljettajakoulutus' and koodiarvo='6';
