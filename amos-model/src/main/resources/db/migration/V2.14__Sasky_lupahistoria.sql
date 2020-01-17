4ee59471aaefc3c42f8f4d2b94a6877bb0159cb1
Jermu Mäki <jermu.maki@netum.fi>
2020-01-17 16:42:23 +0200

    Modified SASKY lupa histoy dates to correct values

---
+++ amos-model/src/main/resources/db/migration/V2.14__Sasky_lupahistoria.sql
@@ -0,0 +1,3 @@
+update lupahistoria set voimassaololoppupvm='2018-07-31' where diaarinumero='60/531/2017' and ytunnus='0204964-1';
+update lupahistoria set oid='1.2.246.562.10.83391443328' where ytunnus='0204964-1';
+update lupahistoria set paatospvm='2018-06-19' where diaarinumero='7/531/2018' and ytunnus='0204964-1';
