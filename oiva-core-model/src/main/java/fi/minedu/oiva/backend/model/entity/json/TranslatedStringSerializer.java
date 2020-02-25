6ad634925a10e2c00739f7a84f17132ec8ed053e
Valtteri Rouvinen <valtteri.rouvinen@valamis.com>
2020-02-25 14:58:32 +0200

    Revert: CSCOIVA-1134 Added cache for LupaService getByYtunnus method.
    - Fixed cache configs to use jackson serialization instead of default.

--- oiva-core-model/src/main/java/fi/minedu/oiva/backend/model/entity/json/TranslatedStringSerializer.java
+++ oiva-core-model/src/main/java/fi/minedu/oiva/backend/model/entity/json/TranslatedStringSerializer.java
@@ -3,7 +3,6 @@ package fi.minedu.oiva.backend.model.entity.json;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import fi.minedu.oiva.backend.model.entity.TranslatedString;
 
 import java.io.IOException;
@@ -19,16 +18,5 @@ public class TranslatedStringSerializer extends JsonSerializer<TranslatedString>
             } catch (IOException ignore) {}
         });
         jgen.writeEndObject();
-    }
-
-    @Override
-    public void serializeWithType(TranslatedString value, JsonGenerator gen, SerializerProvider serializers, TypeSerializer typeSer) throws IOException {
-        typeSer.writeTypePrefixForObject(value, gen);
-        value.stream().forEach(e -> {
-            try {
-                gen.writeStringField(e.getKey(), e.getValue());
-            } catch (IOException ignore) {}
-        });
-        typeSer.writeTypeSuffixForObject(value, gen);
     }
 }
