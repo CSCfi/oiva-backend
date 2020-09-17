6a751b16a8079bfaf06ebb705c95bb112421a4fe
jleppnen <jukka.leppanen@dicode.fi>
2020-09-17 14:14:35 +0300

    CSCOIVA-1525: Lisätty endpoint liitteen poistamiselle uuid:lla

--- oiva-core/src/test/java/fi/minedu/oiva/backend/core/web/controller/BaseLiiteControllerIT.java
+++ oiva-core/src/test/java/fi/minedu/oiva/backend/core/web/controller/BaseLiiteControllerIT.java
@@ -2,7 +2,9 @@ package fi.minedu.oiva.backend.core.web.controller;
 
 import com.jayway.jsonpath.DocumentContext;
 import fi.minedu.oiva.backend.core.it.BaseIT;
+import fi.minedu.oiva.backend.model.security.annotations.OivaAccess;
 import org.junit.Test;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -30,5 +32,27 @@ public abstract class BaseLiiteControllerIT extends BaseIT {
         log.info(doc.jsonString());
         assertEquals("Testi_liite_2", doc.read("$.nimi"));
         assertFalse(doc.read("$.salainen", Boolean.class));
+    }
+
+    @Test
+    public void deleteNoAccessForKayttaja() {
+        loginAs("user1", okmOid, OivaAccess.Context_Kayttaja);
+        makeRequest(HttpMethod.DELETE,
+                "/api/liitteet/cc3962e0-43b6-11e8-b2ef-005056aa0e61", null, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @Test
+    public void delete() {
+        String uuid = "cc3962e0-43b6-11e8-b2ef-005056aa0e61";
+        loginAs("testEsittelija", okmOid, OivaAccess.Context_Esittelija);
+        makeRequest(HttpMethod.DELETE, "/api/liitteet/" + uuid, null, HttpStatus.OK);
+        makeRequest("/api/liitteet/" + uuid, HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void deleteNotFound() {
+        loginAs("testEsittelija", okmOid, OivaAccess.Context_Esittelija);
+        makeRequest(HttpMethod.DELETE,
+                "/api/liitteet/d190a7be-f8d4-11ea-adc1-0242ac120002", null, HttpStatus.NOT_FOUND);
     }
 }
