2d2aaf4958c79b89c3ebc632bae4f87959e57a46
Valtteri Rouvinen <valtteri.rouvinen@valamis.com>
2020-10-15 10:14:17 +0300

    CSCOIVA-1572 Added PO kohde information.
    - Added koulutustyyppi for kohde.

---
+++ amos-backend/src/main/resources/db/migration/V18.1__Perusopetus_kohde_migrations.sql
@@ -0,0 +1,8 @@
+INSERT INTO kohde (tunniste, meta, luoja, koulutustyyppi)
+VALUES ('opetusjotalupakoskee', '{"otsikko": {"fi": "Opetus, jota lupa koskee", "sv": ""}}', 'oiva', 1),
+       ('kunnatjoissaopetustajarjestetaan', '{"otsikko": {"fi": "Kunnat, joissa opetusta järjestetään", "sv": ""}}', 'oiva', 1),
+       ('opetuskieli', '{"otsikko": {"fi": "Opetuskieli", "sv": ""}}', 'oiva', 1),
+       ('opetuksenjarjestamismuoto', '{"otsikko": {"fi": "Opetuksen järjestämismuoto", "sv": ""}}', 'oiva', 1),
+       ('erityinenkoulutustehtava', '{"otsikko": {"fi": "Erityinen koulutustehtävä", "sv": ""}}', 'oiva', 1),
+       ('oppilasopiskelijamaara', '{"otsikko": {"fi": "Oppilas-/Opiskelijamäärä", "sv": ""}}', 'oiva', 1),
+       ('muutkoulutuksenjarjestamiseenliittyvatehdot', '{"otsikko": {"fi": "Muut koulutuksen järjestämiseen liittyvät ehdot", "sv": ""}}', 'oiva', 1);
