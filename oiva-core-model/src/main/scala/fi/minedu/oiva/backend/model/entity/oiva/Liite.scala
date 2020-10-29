dd0a6f4fe11579e332a74af27bb72113a8ea788e
Valtteri Rouvinen <valtteri.rouvinen@valamis.com>
2020-10-29 15:11:23 +0200

    CSCOIVA-1548 Added luoja to liite json.

--- oiva-core-model/src/main/scala/fi/minedu/oiva/backend/model/entity/oiva/Liite.scala
+++ oiva-core-model/src/main/scala/fi/minedu/oiva/backend/model/entity/oiva/Liite.scala
@@ -18,7 +18,6 @@ class Liite() extends pojos.Liite  {
 
     // exclude from json
     @JsonIgnore override def getId: lang.Long = super.getId
-    @JsonIgnore override def getLuoja: String = super.getLuoja
     @JsonIgnore override def getPaivittaja: String = super.getPaivittaja
     @JsonIgnore override def getPaivityspvm: Timestamp = super.getPaivityspvm
 
