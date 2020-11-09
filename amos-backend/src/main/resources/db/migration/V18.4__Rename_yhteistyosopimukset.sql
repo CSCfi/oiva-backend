314494885fc2ef8728e3288bc79569b9db1a3883
jleppnen <jukka.leppanen@dicode.fi>
2020-11-09 12:48:58 +0200

    CSCOIVA-1610: Yhteistyösopimusmääräyksien metatietojen muutos

---
+++ amos-backend/src/main/resources/db/migration/V18.4__Rename_yhteistyosopimukset.sql
@@ -0,0 +1,14 @@
+-- Create attribute yhteistyosopimus
+update maarays
+    SET meta = jsonb_set(meta, '{yhteistyosopimus}', '{}')
+where maarays.koodiarvo = '8' and maarays.meta->>'yhteistyösopimus' IS NOT NULL;
+
+-- Create attribute yhteistyosopimus->kuvaus with value copied from yhteistyösopimus->fi
+UPDATE maarays
+SET meta = jsonb_set(meta, '{yhteistyosopimus, kuvaus}', meta->'yhteistyösopimus'->'fi', true)
+where maarays.koodiarvo = '8' and maarays.meta->>'yhteistyösopimus' IS NOT NULL;
+
+--Delete attribute yhteistyösopimus
+UPDATE maarays
+SET meta = maarays.meta::jsonb - 'yhteistyösopimus'
+where maarays.koodiarvo = '8' and maarays.meta->>'yhteistyösopimus' IS NOT NULL;
