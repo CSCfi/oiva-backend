8dc4eacc21e41d4b6909f43c65d841f2898ff67d
Valtteri Rouvinen <valtteri.rouvinen@valamis.com>
2021-01-12 11:41:04 +0200

    CSCOIVA-1638 Added kohde data for lukio.

---
+++ amos-backend/src/main/resources/db/migration/V21.12__Lukio_kohde_migrations.sql
@@ -0,0 +1,9 @@
+INSERT INTO kohde (tunniste, meta, luoja, koulutustyyppi)
+VALUES ('kunnatjoissaopetustajarjestetaan', '{"otsikko": {"fi": "Kunnat, joissa opetusta järjestetään", "sv": ""}}', 'oiva', 2),
+       ('opetuskieli', '{"otsikko": {"fi": "Opetuskieli", "sv": ""}}', 'oiva', 2),
+       ('sisaoppilaitosmuotoinenkoulutus', '{"otsikko": {"fi": "Oikeus sisäoppilaitosmuotoiseen koulutukseen", "sv": ""}}', 'oiva', 2),
+       ('erityinenkoulutustehtava', '{"otsikko": {"fi": "Erityinen koulutustehtävä", "sv": ""}}', 'oiva', 2),
+       ('valtakunnallinenkehittamistehtava', '{"otsikko": {"fi": "Valtakunnallinen kehittämistehtävä", "sv": ""}}', 'oiva', 2),
+       ('opiskelijamaarat', '{"otsikko": {"fi": "Opiskelijamäärät", "sv": ""}}', 'oiva', 2),
+       ('muutkoulutuksenjarjestamiseenliittyvatehdot', '{"otsikko": {"fi": "Muut koulutuksen järjestämiseen liittyvät ehdot", "sv": ""}}', 'oiva', 2),
+       ('liitetiedostot', '{"otsikko": {"fi": "Liitetiedostot", "sv": ""}}', 'oiva', 2);
